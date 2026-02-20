7e9c98cc7ad4a286451134d13c2962735ba3a16d
RafaAlhumud <csrafamousa@gmail.com>
2026-02-20 21:26:27 +0300

    Lab 2: updated lab2 work modules

--- bookwebsite/settings.py
+++ bookwebsite/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.bookmodule',
+    'apps.usermodule',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'bookwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bookwebsite/urls.py
+++ bookwebsite/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+import apps.bookmodule.views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# urlpatterns = [
+#    path('admin/', admin.site.urls),
+#   path('home', include('apps.bookmodule.urls')),
+#]
+
+#urlpatterns = [ 
+#path('admin/', admin.site.urls), 
+#path('', apps.bookmodule.views.index),  #add only this line of code
+#path('index2/<int:val1>/', apps.bookmodule.views.index2),
+#]
+
+urlpatterns = [ 
+path('admin/', admin.site.urls), 
+path('books/', include("apps.bookmodule.urls")), 
+path('users/', include("apps.usermodule.urls")),  
+] 
