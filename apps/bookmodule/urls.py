5415e20021e33341528bf33f55bf056f79d3820e
RafaAlhumud <csrafamousa@gmail.com>
2026-04-19 05:31:27 +0300

    lab7

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -20,5 +20,7 @@ path('html5/text/formatting', views.formatting, name='formatting'),
 path('html5/listing', views.listing, name='listing' ),
 path('html5/tables', views.tables, name='tables'),
 path('search/', views.search_books, name='search_books'),
+path('simple/query', views.simple_query, name='simple_query'),
+path('complex/query', views.complex_query, name='complex_query'),
 ] 
 
